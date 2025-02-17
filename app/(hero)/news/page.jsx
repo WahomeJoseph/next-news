4eab568025fe503b74125bdc3a0a3d3e39acd192
wahomeJohn <johnweru206@gmail.com>
2025-02-17 16:03:03 +0300

    client side data fetching

--- app/(hero)/news/page.jsx
+++ app/(hero)/news/page.jsx
@@ -1,11 +1,43 @@
-import { DUMMY_NEWS } from '@/News'
+'use client'
 import NewsList from '@/components/NewsList'
+import { useEffect, useState } from 'react'
 
 export default function News() {
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState()
+  const [news, setNews] = useState()
+
+  useEffect(() => {
+    async function fetchNews() {
+      setIsLoading(true)
+      const result = await fetch('http://localhost:8080/news')
+      const data = await result.json()
+
+      setIsLoading(false)
+      setNews(data)
+
+      if (!result.ok) {
+        setError('Failed To Fetch News!')
+        setIsLoading(false)
+      }
+    }
+    fetchNews()
+  }, [])
+
+  { isLoading && <p>Loading...</p> }
+
+  if (error) {
+    return <p>{error}</p>
+  }
+
+  let newsContent
+  if (news) {
+    newsContent = <NewsList news={news} />
+  }
   return (
     <>
-      <h4 className=" text-center text-3xl tracking-wide text-[#ddd6cb] font-montserrat font-bold mt-5">News Cards</h4>
-      <NewsList news={DUMMY_NEWS} />
+      <h2 className=" text-center text-3xl tracking-wide text-[#ddd6cb] font-montserrat font-bold mt-5">News Cards</h2>
+      {newsContent}
     </>
   )
 }
